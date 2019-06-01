d06d2530fe9dfc9dca929aa5fb5abfcdb26c54f5
Arka Ariza <arjarizami@unal.edu.co>
2019-06-01 13:51:15 -0500

    Restaurants api connection

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,20 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const apiAdapter = require('../config/apiAdapter');
+const apiAdapter = require("../config/apiAdapter");
 
-const BASE_URL = 'http://localhost:5001';
+const BASE_URL = "http://localhost:5001";
 const api = apiAdapter(BASE_URL);
 
-router.get('/restaurants', (req, res) => {
-    api.get(req.path).then(resp => {
-        res.send(resp.data)
-    });
+router.get("/restaurants", (req, res) => {
+  api.get(req.originalUrl).then(resp => {
+    res.send(resp.data);
+  });
 });
 
-router.get('/restaurants/:id', (req, res) => {
-    api.get(req.path).then(resp => {
-        res.send(resp.data)
-    });
+router.get("/restaurants/:id", (req, res) => {
+  api.get(req.originalUrl).then(resp => {
+    res.send(resp.data);
+  });
+});
+
+router.post("/restaurants/add", (req, res) => {
+  api.post(req.originalUrl).then(resp => {
+    res.send(resp.data);
+  });
+});
+
+router.put("/restaurants/update/:id", (req, res) => {
+  api.put(req.originalUrl).then(resp => {
+    res.send(resp.data);
+  });
+});
+
+router.delete("/restaurants/delete/:id", (req, res) => {
+  api.delete(req.originalUrl).then(resp => {
+    res.send(resp.data);
+  });
 });
 
 module.exports = router;
