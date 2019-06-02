f746c7ceb1ea5134803179986e12ba6c9eeee183
Arka Ariza <arjarizami@unal.edu.co>
2019-06-02 18:00:11 -0500

    a

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var router = require("./routes/router");
 const bodyParser = require("body-parser");
 const MyGraphQLSchema = require("./graphQL/graphQL");
 
+const cors = require("cors");
+
 const getRestaurant = require("./services/restaurant");
 
 app.use(bodyParser.json());
@@ -13,6 +15,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require("./routes/index")(app);
 
 app.use(router);
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(
   "/graphql",
