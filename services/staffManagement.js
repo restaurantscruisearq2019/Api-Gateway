66aa955f17368ee1ed12d176ac45bc07f84c35e7
miacastroco <miacastroco@unal.edu.co>
2019-06-02 22:05:33 -0500

    Staff Management Query

--- services/staffManagement.js
+++ services/staffManagement.js
@@ -1,9 +1,33 @@
 const axios = require("axios");
 
 const getStaffManagers = async () => {
-    const url = "http://localhost:5002/managers";
-    const res = await axios.get(url);
-    return res.data.data;
+    const managersUrl = "http://localhost:5002/managers";
+    const employeesUrl = "http://localhost:5002/employees";
+    const managers = await axios.get(managersUrl);
+    const employees = await axios.get(employeesUrl);
+    
+    const managerEmployees = employees.data.data.filter( e =>{
+      return managers.data.data.filter( m => {
+        return m.employees.filter( eID => {
+          return eID === e._id;
+        })
+      })
+    });
+
+    var managersObj = [];
+  
+    managers.data.data.filter(m => {
+        managersObj.push({
+          name: m.name,
+          _id: m._id,
+          userName: m.userName,
+          password: m.password,
+          restaurantid: m.restaurantid,
+          employees: managerEmployees
+      })
+    });
+
+    return managersObj;
 };
 
 const getStaffEmployees = async () => {
