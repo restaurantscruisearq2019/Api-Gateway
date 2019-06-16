6dad9486883c2ca9807606656ecc889a21f579a7
miacastroco <miacastroco@unal.edu.co>
2019-06-16 01:29:13 -0500

    Update apiRoutes.js

--- config/apiRoutes.js
+++ config/apiRoutes.js
@@ -1,15 +1,15 @@
-const reservationURL = "http://localhost:5001"; //To edit
+const reservationURL = "http://ec2-3-89-70-91.compute-1.amazonaws.com:5001"; //To edit
 const clientsURL = reservationURL+"/clients";
 const groupsURL = reservationURL+"/groups";
 const reservedgroupsURL = reservationURL+"/reservedgroups";
 
-const restaurantsURL = "http://localhost:5002/restaurants"; //To edit
+const restaurantsURL = "http://ec2-184-72-208-137.compute-1.amazonaws.com:5002/restaurants"; //To edit
 
-const staffManagementURL = "http://localhost:5003"; //To edit
+const staffManagementURL = "http://ec2-18-212-176-232.compute-1.amazonaws.com:5003"; //To edit
 const managersURL = staffManagementURL+"/managers";
 const employeesURL = staffManagementURL+"/employees";
 
-const ldap = "http://localhost:5004/login"; //To edit
+const ldap = "http://ec2-18-207-203-122.compute-1.amazonaws.com:5004/login"; //To edit
 
 module.exports = {
     reservationURL,
